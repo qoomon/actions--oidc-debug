c695c4324cb9bb835bdf3c29884a28c365310518
Bengt Brodersen <bengt.brodersen@gmail.com>
2024-03-15 14:37:00 +0100

    Initial commit

---
+++ lib/actions.ts
@@ -0,0 +1,82 @@
+import * as core from '@actions/core'
+import * as httpClient from '@actions/http-client'
+import * as _exec from '@actions/exec'
+import YAML from 'yaml'
+
+/**
+ * Run action and catch errors
+ * @param action - action to run
+ * @returns void
+ */
+export function run(action: () => Promise<void>): void {
+  action().catch(async (error: unknown) => {
+    core.setFailed('Unhandled error, see job logs')
+    console.error('Error:', error)
+    if (error instanceof httpClient.HttpClientError) {
+      console.error('Http response:', error.result)
+    }
+  })
+}
+
+/**
+ * Gets string value of an input.
+ * Unless trimWhitespace is set to false in InputOptions, the value is also trimmed.
+ * Returns null if the value is not defined.
+ *
+ * @param     name     name of the input to get
+ * @param     options  optional. See InputOptions.
+ * @returns   parsed input as object
+ */
+export function getInput(name: string, options?: core.InputOptions): string | null {
+  return core.getInput(name, options) || null
+}
+
+/* eslint-disable  @typescript-eslint/no-explicit-any */
+/**
+ * Gets the yaml value of an input.
+ * Unless trimWhitespace is set to false in InputOptions, the value is also trimmed.
+ * Returns null if the value is not defined.
+ *
+ * @param     name     name of the input to get
+ * @param     options  optional. See InputOptions.
+ * @returns   parsed input as object
+ */
+export function getYamlInput(name: string, options?: core.InputOptions): any | null {
+  const input = getInput(name, options)
+  if (input === null) return null
+  return YAML.parse(input)
+}
+
+/**
+ * Execute a command and get the output.
+ * @param commandLine - command to execute (can include additional args). Must be correctly escaped.
+ * @param args - optional command arguments.
+ * @param options - optional exec options. See ExecOptions
+ * @returns status, stdout and stderr
+ */
+export async function exec(commandLine: string, args?: string[], options?: _exec.ExecOptions): Promise<ExecResult> {
+  const stdoutChunks = <Buffer[]>[]
+  const stderrChunks = <Buffer[]>[]
+  const status = await _exec.exec(commandLine, args, {
+    ...options,
+    listeners: {
+      stdout(data) {
+        stdoutChunks.push(data)
+      },
+      stderr(data) {
+        stderrChunks.push(data)
+      },
+    },
+  })
+  return {
+    status,
+    stdout: Buffer.concat(stdoutChunks as Uint8Array[]),
+    stderr: Buffer.concat(stderrChunks as Uint8Array[]),
+  }
+}
+
+export type ExecResult = {
+  status: number
+  stdout: Buffer
+  stderr: Buffer
+}
